1cb16a0628dce70ac810575c4b3f9dc2eb749f31
Oswaldo de Jesus Chacon Perez <39079647+OswaldoChacon@users.noreply.github.com>
2020-10-26 21:19:24 -0600

    migraciones

---
+++ database/migrations/2020_10_27_030231_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->money('precio',9,2);
+            $table->int('stock');
+            $table->bigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categoriass')->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
